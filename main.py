a910bd191422b44888299900d5fa5be470ae5c89
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-11-06 13:07:58 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Ethan Lawrence
+# Nov 6 2024
+# lists for loops
+
+# 4-1 Pizzas
+pizzas = ['pepperoni', 'plain', 'pineapple']
+for pizza in pizzas:
+    print(f'I like {pizza} pizza.')
+print('I realy Like pizza \n')
+
+# 4-2 Animals
+birds = ['eagle', 'falcon', 'crow', 'vulture']
+for bird in birds:
+    print(f'The {bird} is a magestic bird.')
+print('All these animals are fantastic birds')
